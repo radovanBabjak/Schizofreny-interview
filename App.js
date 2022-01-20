1858187eb9d108a57729e917f653a3116d10c61d
radovanBabjak <radovan@saleschamp.nl>
2022-01-20 19:43:37 +0100

    feat: fetch movies

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const getMovies = async () => {
+     try {
+      const response = await fetch('https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/movies.json');
+      const json = await response.json();
+      setMovies(json.movies);
+
+    } catch (error) {
+      setErrorMsg(error.message);
+
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading ? <ActivityIndicator> </ActivityIndicator> : 
+        errorMsg ? <Text>{errorMsg}</Text> : (
+        
+          <FlatList
+            data={movies}
+            keyExtractor={({ episode_number }) => episode_number}
+            renderItem={({ item }) => (
+              <Text>{item.title}</Text>
+            )}
+          />
+        )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
