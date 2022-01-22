cd0e6956d140979a60bf0b60e842f95f002c7f82
radovanBabjak <radovan@saleschamp.nl>
2022-01-22 23:17:01 +0100

    feat: move app to typescript

--- App.tsx
+++ App.tsx
@@ -1,17 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, ReactElement } from 'react';
+import { StyleSheet, ActivityIndicator, FlatList, Text, View, ScrollView } from 'react-native';
+import { EpisodeItem, Button } from './components';
+import { MovieConfig, SortType } from './types';
 
-export default function App() {
+const baseUrl = 'https://raw.githubusercontent.com/RyanHemrick/star_wars_movie_app/master/';
+
+export default function App(): ReactElement {
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const getMovies = async (): Promise<void> => {
+     try {
+      const response = await fetch(baseUrl + 'movies.json');
+      const json = await response.json();
+      setMovies(json.movies);
+
+    } catch (error: any) {
+      setErrorMsg(error.message);
+
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const sortMovies = (isAscending: SortType): () => unknown => {
+
+    return () => setMovies(() => {
+      return [...movies.sort((x: MovieConfig, y: MovieConfig) => {
+        return isAscending === SortType.Ascending ? 
+          x.episode_number - y.episode_number : 
+          y.episode_number - x.episode_number
+      })]
+    })
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.appContainer }>
+      {isLoading ? <ActivityIndicator> </ActivityIndicator> : (
+        <ScrollView>
+          <FlatList
+            data={movies}
+            keyExtractor={({ episode_number }: MovieConfig ) => episode_number.toString() }
+
+            ListEmptyComponent={<Text>{ errorMsg }</Text>}
+            
+            renderItem={({ item }: {item: MovieConfig}) => (
+              <EpisodeItem
+                titleWithEpisode={ item.title }
+                episodeNumber={ item.episode_number }
+                imageSrc={{uri: baseUrl + 'public/images/' + item.poster }}
+              />
+            )}
+          >
+          </FlatList>
+
+          <View>
+            <Button 
+              backgroundColor={"#fca311"}
+              description="sort ascendingly" 
+              onPress={sortMovies(SortType.Ascending)}
+            />
+
+            <Button 
+              backgroundColor={"#14213d"}
+              description="sort descendingly" 
+              onPress={sortMovies(SortType.Descending)}
+            />
+          </View>
+        </ScrollView>
+      )}
     </View>
   );
-}
+
+};
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
---
+++ types/types.ts
@@ -0,0 +1,10 @@
+export interface MovieConfig {
+    episode_number: number,
+    title: string,
+    poster: string,
+};
+
+export enum SortType {
+    Ascending,
+    Descending,
+}
